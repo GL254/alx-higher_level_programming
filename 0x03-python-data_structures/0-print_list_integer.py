8af79a8aaed9fba2586132de48c0d68bdf0c10e8
GL254 <kikwaig4@gmail.com>
2023-08-14 12:39:09 -0700

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    for item in my_list:
+        print("{:d}".format(item))
+        """ Prints all the intergers of a list"""
