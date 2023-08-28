02509f7f41585909ee986eba52259e45ad9d420b
GL254 <kikwaig4@gmail.com>
2023-08-28 04:51:29 -0700

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x elements of a list and only integers"""
+    i = 0
+    output = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            output += 1
+            except (ValueError, TypeError):
+                continue
+            print("")
+            return (output)
